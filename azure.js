dc4bd7374b3ee170b25ac2f397518c7d67d984a5
rotamtechnologies <contacto@romtamfuel.com>
2020-08-13 11:56:39 -0400

    prueba

--- azure.js
+++ azure.js
@@ -57,7 +57,7 @@ function leer(dataArray, c) {
 }
 
 
-var port = process.env.PgT || 3001;
+var port = process.env.PORT || 3001;
 
 server.listen(port, function () {
     console.log('listening in http://localhost:' + port);
