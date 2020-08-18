7b303bee34aa8c585a75f055e0a0bbb81758b4a6
rotamtechnologies <contacto@romtamfuel.com>
2020-08-18 19:26:14 -0400

    se limpia para generar endpoint keycloak

--- azure.js
+++ azure.js
@@ -1,4 +1,4 @@
-//process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 const {EventHubConsumerClient} = require("@azure/event-hubs");
 var k = require( 'keycloak-admin');
@@ -37,7 +37,6 @@ httpApp.get('/', function (req, res) {
 
     res.send('Hello World!');
 });
-
 kcAdminClient.auth({})
 
 
