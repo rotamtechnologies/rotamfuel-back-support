3ccd0ecd14f118ba61e0c7a5b43f70eaf85e0c71
rotamtechnologies <contacto@romtamfuel.com>
2020-08-30 18:06:44 -0400

    se agregan metodos keycloak

--- src/util/httpRequester.js
+++ src/util/httpRequester.js
@@ -5,13 +5,12 @@ global.HttpRequester = {
     makePOST: (url, data) => {
         console.log("REALIZANDO POST")
         return new Promise((ok, nulo) => {
-            ok("ok")
-           /* request.post(url, data, (error, response, body) => {
+            request.post(url, data, (error, response, body) => {
                 if (error != null)
                     nulo(error)
                 else
                     ok(body)
-            })*/
+            })
         })
     }
 }
