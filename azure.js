4ec367eec89e6756499a8f42ed3dd8b7d28e2ad4
rotamtechnologies <contacto@romtamfuel.com>
2020-08-30 14:01:26 -0400

    separando kc y azure

--- azure.js
+++ azure.js
@@ -1,91 +1,34 @@
 //process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
-
-const {EventHubConsumerClient} = require("@azure/event-hubs");
-var k = require( 'keycloak-admin');
-const connectionString = process.env.CONNECTIONSTRING
-const eventHubName = process.env.HUBNAME;
-const consumerGroup = process.env.CONSUMERGROUP;
+const k = require('keycloak-admin');
 const fs = require('fs');
-
 const kcAdminClient = new k.default({
-    baseUrl:'https://app.rotamfuel.com/auth',
-    realmName:'rotamrealm',
-    requestConfig:{
-    }
-
+    baseUrl: process.env.KCBASEURL,
+    realmName: process.env.KCREALMNAME,
+    requestConfig: {}
 });
-
 let app = require('express')();
-
+const port = process.env.PORT
 let laKey = fs.readFileSync(`${process.env.KEYNODE}`, 'utf8')
 let laPassPhrase = `${process.env.PASSPHRASE}`
 let laCert = fs.readFileSync(`${process.env.CERTNODE}`, 'utf8')
-
 let server = require('https').createServer({
-    key: laKey ,
+    key: laKey,
     cert: laCert,
     passphrase: laPassPhrase,
 }, app);
-let elHttp = require('express')
-
-var httpApp = elHttp();
-
-httpApp.get('/', function (req, res) {
-    io.emit('message', req.query.vel)
-
-    res.send('Hello World!');
-});
-
-
 kcAdminClient.auth({
     username: process.env.nodeKEYCLOAK_user,
     password: process.env.nodeKEYCLOAK_password,
     grantType: process.env.nodeKEYCLOAK_grantType,
     clientId: process.env.nodeKEYCLOAK_clientId,
-}).then(dd=>{
+}).then(dd => {
     console.log(dd)
-    kcAdminClient.users.find().then(d=>{
+    kcAdminClient.users.find().then(d => {
         console.log(d)
     })
 
 });
 
-
-
-var port = process.env.PORT || 3001;
-
-/*server.listen(port, function () {
+server.listen(port, function () {
     console.log('listening in http://localhost:' + port);
-});*/
-
-/*httpApp.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});*/
-async function main() {
-
-    const consumerClient = new EventHubConsumerClient(consumerGroup, connectionString, eventHubName);
-
-    // Subscribe to the events, and specify handlers for processing the events and errors.
-    const subscription = consumerClient.subscribe({
-            processEvents: async (events, context) => {
-                for (const event of events) {
-                    //console.log(".")
-                    console.log(`Received event: '${JSON.stringify(event)}' from partition: '${context.partitionId}' and consumer group: '${context.consumerGroup}'`);
-                    io.emit('message', event)
-                }
-                // Update the checkpoint.
-                await context.updateCheckpoint(events[events.length - 1]);
-            },
-
-            processError: async (err, context) => {
-                console.log(`Error : ${err}`);
-            }
-        }
-    );
-
-}
-
-main().catch((err) => {
-    console.log("Error occurred: ", err);
 });
-
