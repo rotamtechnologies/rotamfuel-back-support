1a3c10860f40e2fe4a0238851c117c0712540956
rotamtechnologies <contacto@romtamfuel.com>
2020-08-30 14:01:58 -0400

    separando kc y azure

--- azure.js
+++ azure.js
@@ -1,4 +1,4 @@
-//process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 const k = require('keycloak-admin');
 const fs = require('fs');
 const kcAdminClient = new k.default({
