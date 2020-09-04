e46641c190ed93988c8f413ccbc9370506d33a5b
rotamtechnologies <contacto@romtamfuel.com>
2020-09-04 19:37:16 -0400

    problemas con cors

--- src/middleware/Middleware.js
+++ src/middleware/Middleware.js
@@ -26,6 +26,7 @@ class Middleware{
     agregarCors(){
         this.ExpressApp.use((req, res, next) => {
             res.set("Access-Control-Allow-Origin", "*")
+            res.set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
             res.set("Access-Control-Allow-Headers", "content-type")
             next()
         })
