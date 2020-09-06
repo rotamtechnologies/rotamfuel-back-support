5640382fb7bc06e2a2297687d80dacd9a4f84dd9
rotamtechnologies <contacto@romtamfuel.com>
2020-09-06 19:47:22 -0300

    agregando userinfo

--- src/util/Utils.js
+++ src/util/Utils.js
@@ -3,12 +3,11 @@ const atob = require("atob");
 
 
 global.idByToken = (tkn) => {
-    console.log(atob(tkn.split(".")[1]));
     let idUser = JSON.parse(atob(tkn.split(".")[1])).sub;
     return idUser;
-}
+};
 global.tokenByReq =(req,res)=>{
     var cookies = new Cookies(req, res);
     let token = req.headers.authorization ? req.headers.authorization.substring("Bearer ".length, req.headers.authorization.length) : cookies.get("RTM_FL-tkn");
     return token
-}
+};
