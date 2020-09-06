37afbf75056a1e8dbc031b9e660d5eb3fa0d7f86
rotamtechnologies <contacto@romtamfuel.com>
2020-09-06 12:05:09 -0300

    se modifica update

--- src/middleware/Middleware.js
+++ src/middleware/Middleware.js
@@ -37,8 +37,8 @@ class Middleware {
     agregarOAuth() {
         this.ExpressApp.use((req, res, next) => {
             console.log("Oauth")
-
-            var cookies = new Cookies(req, res)
+            var cookies = new Cookies(req, res);
+            console.log(req.headers);
             let token = req.headers.authorization ? req.headers.authorization : cookies.get("RTM_FL-tkn");
             console.log(token);
             if (token) {
