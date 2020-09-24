5f2353a3fe7d1c3c0cfa90875cb53d12850b3a34
rotamtechnologies <contacto@romtamfuel.com>
2020-09-24 17:38:01 -0300

    se agrega enpoint para obtener Access Token

--- src/services/ThingsService.js
+++ src/services/ThingsService.js
@@ -1,5 +1,5 @@
 require("../util/httpRequester");
-require ("../constants/config")
+require("../constants/config")
 let url = "http://23.98.131.148:8080/api/device";
 let urlToken = "http://23.98.131.148:8080/api/auth/login";
 let urlDispositivo = "http://23.98.131.148:8080/api/device/";
@@ -21,32 +21,29 @@ global.agregarDispositivo = async (data) => {
     token = JSON.parse(token).token
     console.log(token)
     let dispositivo;
-    try{
-         dispositivo =  await HttpRequester.makePOST(url, {
-            headers: {"content-type": "application/json","X-Authorization":"Bearer "+token},
-            body: datosJSON
-        })
-    }catch (e) {
-        console.log(e)
+    dispositivo = await HttpRequester.makePOST(url, {
+        headers: {"content-type": "application/json", "X-Authorization": "Bearer " + token},
+        body: datosJSON
+    })
 
-    }
-    console.log(dispositivo)
+    console.log(JSON.parse(dispositivo).status)
 
     let idDispositivo = JSON.parse(dispositivo).id.id
 
     console.log(idDispositivo)
 
-    let accessToken = await HttpRequester.makeGET(urlDispositivo+dispositivo+"/credentials")
+    let accessToken = await HttpRequester.makeGET(urlDispositivo + dispositivo + "/credentials")
     console.log(accessToken)
 
 };
 
-function obtenerTokenThings(){
-    let datos =`{"username":"${CONFIG.THINGSUSER}", "password":"${CONFIG.THINGSPASSWORD}"}`
+function obtenerTokenThings() {
+    let datos = `{"username":"${CONFIG.THINGSUSER}", "password":"${CONFIG.THINGSPASSWORD}"}`
     console.log(datos)
-    return HttpRequester.makePOST(urlToken,{body:datos})
+    return HttpRequester.makePOST(urlToken, {body: datos})
 }
-function obtenerAccessToken(dispositivo){
-    return HttpRequester.makeGET(urlDispositivo+dispositivo+"/credentials")
+
+function obtenerAccessToken(dispositivo) {
+    return HttpRequester.makeGET(urlDispositivo + dispositivo + "/credentials")
 }
 
