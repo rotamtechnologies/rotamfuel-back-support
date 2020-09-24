7871e62d4a8e628af470885429c36b6623aae33f
rotamtechnologies <contacto@romtamfuel.com>
2020-09-24 19:51:59 -0300

    se agrega enpoint para obtener Access Token

--- src/controllers/ThingsController.js
+++ src/controllers/ThingsController.js
@@ -3,14 +3,15 @@ require("../services/ThingsService")
 router.post("/",(req,res)=>{
     agregarDispositivo(req.body.vin).then(ok=>{
         let token = JSON.parse(ok).credentialsId
+        res.set("content-type","text/plain")
         res.send(token)
     })
 });
-router.get("/token/:vin",(req,res)=>{
+/*router.get("/token/:vin",(req,res)=>{
     console.log(req.params)
     console.log(req.query)
     res.send(req.params.vin)
-});
+});*/
 
 
 
