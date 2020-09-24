946448c657680b9731451f14e9dd8ad281b4ce85
rotamtechnologies <contacto@romtamfuel.com>
2020-09-24 17:36:35 -0300

    se agrega enpoint para obtener Access Token

--- src/services/ThingsService.js
+++ src/services/ThingsService.js
@@ -3,6 +3,7 @@ require ("../constants/config")
 let url = "http://23.98.131.148:8080/api/device";
 let urlToken = "http://23.98.131.148:8080/api/auth/login";
 let urlDispositivo = "http://23.98.131.148:8080/api/device/";
+let urlBuscarIdDispositivo = "http://23.98.131.148:8080/api/tenant/devices?deviceName=";
 
 global.agregarDispositivo = async (data) => {
     let datosJSON = `
@@ -19,11 +20,16 @@ global.agregarDispositivo = async (data) => {
     let token = await obtenerTokenThings()
     token = JSON.parse(token).token
     console.log(token)
+    let dispositivo;
+    try{
+         dispositivo =  await HttpRequester.makePOST(url, {
+            headers: {"content-type": "application/json","X-Authorization":"Bearer "+token},
+            body: datosJSON
+        })
+    }catch (e) {
+        console.log(e)
 
-    let dispositivo =  await HttpRequester.makePOST(url, {
-        headers: {"content-type": "application/json","X-Authorization":"Bearer "+token},
-        body: datosJSON
-    })
+    }
     console.log(dispositivo)
 
     let idDispositivo = JSON.parse(dispositivo).id.id
