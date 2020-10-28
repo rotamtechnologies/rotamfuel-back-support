02db65ecab14c66cc0848a2582fab11318bafa9c
rotamtechnologies <contacto@romtamfuel.com>
2020-10-28 11:29:03 -0300

    se actualiza endpoint para actualizar datos

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -9,26 +9,23 @@ router.get('/descargar/datos', function (req, res) {
 });
 
 router.get("/", (req, res) => {
-    let tokenPeticion = tokenByReq(req,res);
-    if(tokenPeticion){
+    let tokenPeticion = tokenByReq(req, res);
+    if (tokenPeticion) {
         let idUser = idByToken(tokenPeticion);
-        keyCloakClient.usuario(idUser).then(info=>{
-            JSONResponse.OK(res,{id:idUser,datos:info});
+        keyCloakClient.usuario(idUser).then(info => {
+            JSONResponse.OK(res, {id: idUser, datos: info});
         })
-    }else{
-        JSONResponse.ERROR(res,"error token")
+    } else {
+        JSONResponse.ERROR(res, "error token")
     }
 });
 
 
 router.post("/actualizar", (req, res) => {
-    console.log(req.body);
-    let id = req.body.id;
-    delete req.body.id;
-    console.log(req.body);
-    /* keyCloakClient.updateUser(id,req.body).then(d=>{
-         res.send(d);
-     })*/
+
+    keyCloakClient.updateUser(idByToken(tokenByReq(req, res)), req.body).then(d => {
+        res.send(d);
+    })
 });
 
 
