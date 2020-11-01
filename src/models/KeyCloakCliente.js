13a15fce368725a00bab8d25eae2f74d8c2840bf
rotamtechnologies <contacto@romtamfuel.com>
2020-11-01 19:58:43 -0300

    se agrega vin

--- src/models/KeyCloakCliente.js
+++ src/models/KeyCloakCliente.js
@@ -134,7 +134,7 @@ class KeyCloakCliente {
             l = 0
         }
         let newCar = {
-            "regName": "auto" + l,
+            "regName": "auto" + l+Math.floor(Math.random() * (9999 - 1000)) + 1000,
             "VIN": "none",
             "marca": "",
             "modelo": "",
