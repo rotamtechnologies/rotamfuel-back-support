2ffc352d031e629e69176d5f86f580b2a9bda48d
codificatuidea <ceo@codificatuidea.com>
2022-05-16 13:04:17 -0500

    Develops influx_metrics. Add new kpi vehicle speed.

---
+++ src/metrics/influx/metrics/Speed.js
@@ -0,0 +1,31 @@
+const InfluxBaseMetric = require("../influxBaseMetric");
+const { calculateCTC, calculateDistance } = require("../kpi");
+
+class SpeedMetric extends InfluxBaseMetric {
+  static getMetricId() {
+    return "speed_metric";
+  }
+
+  buildMetricParams() {
+    return {
+      _measurements: ["vehicle_speed"],
+    };
+  }
+
+  buildMetricResponse(datapoints) {
+    let speedData = [];
+    datapoints.forEach(datapoint => {
+      // TODO - Francisco must validate this values
+      if (datapoint._value < 100) {
+        speedData.push({
+          x: datapoint._time,
+          y: datapoint._value
+        });
+      }
+    });
+
+    return super.buildMetricResponse(speedData);
+  }
+}
+
+module.exports = SpeedMetric;
